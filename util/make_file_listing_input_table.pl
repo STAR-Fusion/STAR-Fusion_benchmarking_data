73c5d8016b70f2990e22aab466018e76dd43f809
Brian Haas <brianjohnhaas@gmail.com>
2017-03-07 18:11:34 -0500

    add

---
+++ util/make_file_listing_input_table.pl
@@ -0,0 +1,46 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use Carp;
+
+
+my %converter = (CHIMERASCAN => 'ChimeraScan',
+                 CHIMPIPE => 'ChimPipe',
+                 DEFUSE => 'deFuse',
+                 ERICSCRIPT => 'EricScript',
+                 FUSIONHUNTER => 'FusionHunter',
+                 FUSION_CATCHER_V0994e => 'FusionCatcher',
+                 INFUSION_CF3 => 'InFusion',
+                 JAFFA_ASSEMBLY => 'JAFFA-Assembly',
+                 JAFFA_DIRECT => 'JAFFA-Direct',
+                 JAFFA_HYBRID => 'JAFFA-Hybrid',
+                 MAPSPLICE => 'MapSplice',
+                 NFUSE => 'nFuse',
+                 PRADA => 'PRADA',
+                 SOAP_FUSE => 'SOAP-fuse',
+                 STAR_FUSION_GRCh37v19_FL3_v51b3df4 => 'STAR-Fusion',
+                 TOPHAT_FUSION => 'TopHat-Fusion',
+    );
+
+
+while (<STDIN>) {
+    chomp;
+    my $filename = $_;
+
+    $filename =~ m|/samples/([^/]+)/([^/]+)/| or die "Error, cannot decipher path: $filename";
+
+    my $sample_name = $1;
+    my $prog = $2;
+
+    my $proper_progname = $converter{$prog} or die "Error, cannot find proper prog name for $prog";
+        
+    print join("\t", $sample_name, $proper_progname, $filename) . "\n";
+    
+}
+
+
+exit(0);
+
+
+    
