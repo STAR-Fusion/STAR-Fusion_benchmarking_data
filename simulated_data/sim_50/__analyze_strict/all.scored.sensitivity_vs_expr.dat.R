1bca0064c96f9f72cba192ef577dbad1c37014e0
Brian Haas <brianjohnhaas@gmail.com>
2017-03-15 13:28:47 -0400

    adding results

---
+++ simulated_data/sim_50/__analyze_strict/all.scored.sensitivity_vs_expr.dat.R
@@ -0,0 +1,69 @@
+library(cluster)
+library(Biobase)
+library(qvalue)
+NO_REUSE = F
+
+# try to reuse earlier-loaded data if possible
+if (file.exists("all.scored.sensitivity_vs_expr.dat.RData") && ! NO_REUSE) {
+    print('RESTORING DATA FROM EARLIER ANALYSIS')
+    load("all.scored.sensitivity_vs_expr.dat.RData")
+} else {
+    print('Reading matrix file.')
+    primary_data = read.table("all.scored.sensitivity_vs_expr.dat", header=T, com='', sep="\t", row.names=1, check.names=F)
+    primary_data = as.matrix(primary_data)
+}
+source("/ahg/regev/users/bhaas/seq/bhaas/GIT/trinityrnaseq/Analysis/DifferentialExpression/R/heatmap.3.R")
+source("/ahg/regev/users/bhaas/seq/bhaas/GIT/trinityrnaseq/Analysis/DifferentialExpression/R/misc_rnaseq_funcs.R")
+source("/ahg/regev/users/bhaas/seq/bhaas/GIT/trinityrnaseq/Analysis/DifferentialExpression/R/pairs3.R")
+data = primary_data
+sample_types = colnames(data)
+nsamples = length(sample_types)
+sample_colors = rainbow(nsamples)
+sample_type_list = list()
+for (i in 1:nsamples) {
+    sample_type_list[[sample_types[i]]] = sample_types[i]
+}
+sample_factoring = colnames(data)
+for (i in 1:nsamples) {
+    sample_type = sample_types[i]
+    replicates_want = sample_type_list[[sample_type]]
+    sample_factoring[ colnames(data) %in% replicates_want ] = sample_type
+}
+initial_matrix = data # store before doing various data transformations
+sample_factoring = colnames(data)
+for (i in 1:nsamples) {
+    sample_type = sample_types[i]
+    replicates_want = sample_type_list[[sample_type]]
+    sample_factoring[ colnames(data) %in% replicates_want ] = sample_type
+}
+sampleAnnotations = matrix(ncol=ncol(data),nrow=nsamples)
+for (i in 1:nsamples) {
+  sampleAnnotations[i,] = colnames(data) %in% sample_type_list[[sample_types[i]]]
+}
+sampleAnnotations = apply(sampleAnnotations, 1:2, function(x) as.logical(x))
+sampleAnnotations = sample_matrix_to_color_assignments(sampleAnnotations, col=sample_colors)
+rownames(sampleAnnotations) = as.vector(sample_types)
+colnames(sampleAnnotations) = colnames(data)
+data = as.matrix(data) # convert to matrix
+write.table(data, file="all.scored.sensitivity_vs_expr.dat.dat", quote=F, sep='	');
+if (nrow(data) < 2) { stop("
+
+**** Sorry, at least two rows are required for this matrix.
+
+");}
+if (ncol(data) < 2) { stop("
+
+**** Sorry, at least two columns are required for this matrix.
+
+");}
+sample_dist = dist(t(data), method='euclidean')
+gene_cor = NULL
+gene_dist = dist(data, method='euclidean')
+if (nrow(data) <= 1) { message('Too few genes to generate heatmap'); quit(status=0); }
+hc_genes = hclust(gene_dist, method='complete')
+myheatcol = colorpanel(75, 'black','yellow')
+heatmap_data = data
+pdf("all.scored.sensitivity_vs_expr.dat.genes_vs_samples_heatmap.pdf")
+heatmap.3(heatmap_data, dendrogram='row', Rowv=as.dendrogram(hc_genes), Colv=F, col=myheatcol, scale="none", density.info="none", trace="none", key=TRUE, keysize=1.2, cexCol=1, margins=c(10,10), cex.main=0.75, main=paste("samples vs. features
+", "all.scored.sensitivity_vs_expr.dat" ) )
+dev.off()
